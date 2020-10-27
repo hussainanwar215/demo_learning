39bcbc153355902e314de53acadcde2e828b2b57
hussainanwar215 <altaf@helfertech.net>
2020-10-27 20:02:23 +0500

    one to many relationship

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/leads_aos_quotes_1_Leads.php
@@ -0,0 +1,12 @@
+<?php
+// created: 2020-10-27 15:57:40
+$dictionary["Lead"]["fields"]["leads_aos_quotes_1"] = array (
+  'name' => 'leads_aos_quotes_1',
+  'type' => 'link',
+  'relationship' => 'leads_aos_quotes_1',
+  'source' => 'non-db',
+  'module' => 'AOS_Quotes',
+  'bean_name' => 'AOS_Quotes',
+  'side' => 'right',
+  'vname' => 'LBL_LEADS_AOS_QUOTES_1_FROM_AOS_QUOTES_TITLE',
+);
