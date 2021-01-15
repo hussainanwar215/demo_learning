dd1cf8805ea51fba62512cec37e0cc590d3a54b9
Helfertech <info@helfertech.com>
2021-01-15 18:46:56 +0500

    reverting

--- custom/modules/Accounts/language/en_us.lang.php
+++ custom/modules/Accounts/language/en_us.lang.php
@@ -2,6 +2,4 @@
 // created: 2020-11-05 09:29:12
 $mod_strings = array (
   'LBL_TEST_DROP' => 'test drop',
-  'LBL_AFTER_STASH' => 'after stahing now again change in that file',
-  'LBL_TESTING' => 'TESTING YES',
 );
