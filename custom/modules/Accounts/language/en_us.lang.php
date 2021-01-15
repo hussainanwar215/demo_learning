b7985ddf01b3775ef2f319df798c77e5ebea4103
Helfertech <info@helfertech.com>
2021-01-15 17:34:28 +0500

    nothing just cleaning the directory

--- custom/modules/Accounts/language/en_us.lang.php
+++ custom/modules/Accounts/language/en_us.lang.php
@@ -3,5 +3,5 @@
 $mod_strings = array (
   'LBL_TEST_DROP' => 'test drop',
   'LBL_AFTER_STASH' => 'after stahing now again change in that file',
-  'LBL_TESTING' => 'TESTING YES'
+  'LBL_TESTING' => 'TESTING YES',
 );
